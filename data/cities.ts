8141b00698d2526a80ac6aee6f9fcf9439af442e
bega <begmuhommet.jumayev@gmail.com>
2024-11-04 07:19:38 +0800

    test

--- data/cities.ts
+++ data/cities.ts
@@ -1,5 +1,5 @@
 export const CITIES = [
-  { name: 'Paris', coordinates:  {latitude: 48.85661, longitude: 2.351499 }},
+  { name: 'Paris', coordinates:  {latitude: 48.85661, longitude: 2.351498 }},
   { name: 'Cologne', coordinates:  {latitude: 48.85661, longitude: 2.351499 }},
   { name: 'Brussels', coordinates:  {latitude: 48.85661, longitude: 2.351499 }},
   { name: 'Amsterdam', coordinates:  {latitude: 48.85661, longitude: 2.351499 }},
